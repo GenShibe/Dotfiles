e1cdceafedae5ee7579d730a8e42f0015e546127
Gen Shibe <genshibe@gmail.com>
2024-08-28 14:21:02 -0700

    test

--- configuration.nix
+++ configuration.nix
@@ -73,7 +73,7 @@
     pulse.enable = true;
     # If you want to use JACK applications, uncomment this
     jack.enable = true;
-    wireplumber.enable = true;
+    wireplumber.enable = true;  
 
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
